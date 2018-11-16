314e3517fb8ca2f361dd4528bff1c32da09a9c3b
emaxwell14 <emaxwell1414@hotmail.com>
2018-11-16 14:18:45 +0000

    returning correct values for single number

--- test/services/letterService.spec.js
+++ test/services/letterService.spec.js
@@ -2,6 +2,8 @@ const { assert } = require('chai');
 const { computeLettersForNumber } = require('../../src/services/letterService');
 
 describe('Letter Service', () => {
+  /* ########## ERROR AND TYPE TESTS ########## */
+
   it('should throw an error if no number is passed', () => {
     assert.throws(computeLettersForNumber, Error);
   });
@@ -14,7 +16,55 @@ describe('Letter Service', () => {
     assert.throws(() => computeLettersForNumber('not a number'), Error);
   });
 
-  it('should return a string if a valid number is passed', () => {
-    assert.typeOf(computeLettersForNumber(123), 'string');
+  it('should return an array if a valid number is passed', () => {
+    assert.typeOf(computeLettersForNumber(123), 'array');
+  });
+
+  /* ########## SINGLE NUMBER TESTS ########## */
+
+  it('should return correct strings for number one', () => {
+    assert.deepEqual(computeLettersForNumber(1), []);
+  });
+
+  it('should return correct strings for number two', () => {
+    assert.deepEqual(computeLettersForNumber(2), ['a', 'b', 'c']);
+  });
+
+  it('should return correct strings for number three', () => {
+    assert.deepEqual(computeLettersForNumber(3), ['d', 'e', 'f']);
+  });
+
+  it('should return correct strings for number four', () => {
+    assert.deepEqual(computeLettersForNumber(4), ['g', 'h', 'i']);
+  });
+
+  it('should return correct strings for number five', () => {
+    assert.deepEqual(computeLettersForNumber(5), ['j', 'k', 'l']);
+  });
+
+  it('should return correct strings for number six', () => {
+    assert.deepEqual(computeLettersForNumber(6), ['m', 'n', 'o']);
+  });
+
+  it('should return correct strings for number seven', () => {
+    assert.deepEqual(computeLettersForNumber(7), ['p', 'q', 'r', 's']);
+  });
+
+  it('should return correct strings for number eight', () => {
+    assert.deepEqual(computeLettersForNumber(8), ['t', 'u', 'v']);
+  });
+
+  it('should return correct strings for number nine', () => {
+    assert.deepEqual(computeLettersForNumber(9), ['w', 'x', 'y', 'z']);
+  });
+
+  it('should return correct strings for number zero', () => {
+    assert.deepEqual(computeLettersForNumber(0), [' ']);
+  });
+
+  /* ########## TWO NUMBER TESTS ########## */
+
+  it('should return correct strings for number zero', () => {
+    assert.deepEqual(computeLettersForNumber(22), ['aa', 'ab', 'ac', 'ba', 'bb', 'bc', 'ca', 'cb', 'cc']);
   });
 });
